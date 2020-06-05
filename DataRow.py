27cf27a7cd24c686c53d3620171b4097a805ae1e
Jesus Nieto <jesusnieto55@gmail.com>
2020-06-05 19:27:26 +0200

    Added python script to evaluate images. Removed unused files.

--- DataRow.py
+++
@@ -1,6 +0,0 @@
-class DataRow:
-    def __init__(self, name, raw_images, processed_images, coordinates):
-        self.name = name
-        self.raw_images = raw_images
-        self.processed_images = processed_images
-        self.coordinates = coordinates
